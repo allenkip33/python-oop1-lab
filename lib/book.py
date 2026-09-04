06a1a822fcb1863a38f986c57e21b8652de39f69
Allenkip33 <allenkip32@gmail.com>
2026-09-05 02:05:42 +0300

    first commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self):
+        self.title = input("enter the title :")
+        while True :
+         try:
+            self.page_count = int(input("Enter the page count :"))
+            break
+         except ValueError:
+            print("page_count must be an integer")
+                
     
-        
+    def turn_page (self):
+        print("Flipping the page...wow, you read fast!")
+
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self):
+        self.price =  float(input("Enter the price"))
+
+        while True:
+            size_input = input("Enter the size :").capitalize()
+            if size_input not in ("Small", "Medium", "Large"):
+                print("size must be Small, Medium, or Large")
+            else:
+                self.input = size_input
+                break
+
+    def tip(self):
+            print("This coffee is great, here’s a tip!")
+            self.price += 1
+
+
